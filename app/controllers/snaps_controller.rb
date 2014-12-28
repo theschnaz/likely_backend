62accbce2efbbb91582e21c40e4236a8ce9aac4f
Gregory Schnese <theschnaz@gmail.com>
2014-12-27 20:13:54 -0500

    finishing test

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -16,12 +16,12 @@ class SnapsController < ApplicationController
   end
   
   def get_snap
-    snap = Snap.find_by_id(2)
+    snap = Snap.find_by_id(rand(4..6))
     render :text => 'http://res.cloudinary.com/hh55qpw1c/image/upload/v1419546151/' + snap.id.to_s + '.jpg'
   end
   
   def get_snap_and_vote
-    snap = Snap.find_by_id(3)
+    snap = Snap.find_by_id(rand(4..6))
     render :text => 'http://res.cloudinary.com/hh55qpw1c/image/upload/v1419546151/' + snap.id.to_s + '.jpg'
   end
   
