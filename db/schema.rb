3f6bb48ed5eb0ea179b229263cb66647aa9935b9
Gregory Schnese <theschnaz@gmail.com>
2014-12-30 22:26:17 -0500

    adding votes model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225214036) do
+ActiveRecord::Schema.define(version: 20141231032309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 20141225214036) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_id"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer "user_id"
+    t.integer "snap_id"
   end
 
 end
