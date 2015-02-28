c753fd19566c5da1f913ba8df89dd40acfc0428e
Gregory Schnese <theschnaz@gmail.com>
2015-02-28 15:40:44 -0500

    tweak

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -51,7 +51,7 @@ class SnapsController < ApplicationController
     vote.user_id = user.id
     snap_id = params[:snap_url]
     #this little beauty finds the ID from the URL
-    vote.snap_id = snap_id[61...-4]
+    vote.snap_id = snap_id[85...-4]
     vote.vote = params[:vote]
     vote.save
     
