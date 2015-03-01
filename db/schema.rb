a2a4792f6bca4047115f86983c7b2873904208eb
Gregory Schnese <theschnaz@gmail.com>
2015-03-01 16:20:02 -0500

    adding created and updated at to votes model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131203652) do
+ActiveRecord::Schema.define(version: 20150301211815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(version: 20150131203652) do
   end
 
   create_table "votes", force: true do |t|
-    t.integer "user_id"
-    t.integer "snap_id"
-    t.string  "vote"
+    t.integer  "user_id"
+    t.integer  "snap_id"
+    t.string   "vote"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
