b6158bfc16e6a8966901c80339c7174a67db7e6a
Gregory Schnese <theschnaz@gmail.com>
2015-03-23 22:41:21 -0400

    adding percents to images 2

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -34,7 +34,7 @@ class SnapsController < ApplicationController
   
   def get_snap
     user = User.find_by_uid(params[:uid])
-    snap = Snap.find_by_sql("select * from snaps where id NOT IN (select snap_id from votes where user_id =" + user.id.to_s + ")")
+    snap = Snap.find_by_sql("select id, photo_url, vote_right, vote_left from snaps where id NOT IN (select snap_id from votes where user_id =" + user.id.to_s + ")")
     ##this gets a little wonky if the snap_id in the votes table is blank
     snap = snap.first
     if(snap)
