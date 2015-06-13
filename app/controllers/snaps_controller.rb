a7c98da76a9f4e8616a72f870ae681c9851c020c
Gregory Schnese <theschnaz@gmail.com>
2015-06-13 16:10:03 -0400

    response tweak8

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -3,7 +3,7 @@ class SnapsController < ApplicationController
   
   def new_share_photo
     data = Cloudinary::Uploader.upload(params[:photo])
-    render :text => data[:public_id]
+    render :text => data[0].public_id
   end
  
   def new_snap
