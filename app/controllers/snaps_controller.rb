929c61296b9ab2b4da48c3098ac8e4e35f0b293e
Gregory Schnese <theschnaz@gmail.com>
2015-06-13 15:54:17 -0400

    response tweak4

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -3,7 +3,7 @@ class SnapsController < ApplicationController
   
   def new_share_photo
     Cloudinary::Uploader.upload(params[:photo])
-    render :text => response.url
+    render :text => response.public_id
   end
  
   def new_snap
