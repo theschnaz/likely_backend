8a81e132c1b7d7709826506a7af43857c864a8d3
Gregory Schnese <theschnaz@gmail.com>
2015-06-27 16:23:17 -0400

    tweaking emailer

--- app/controllers/emailresults_controller.rb
+++ app/controllers/emailresults_controller.rb
@@ -1,12 +1,17 @@
 class EmailresultsController < ApplicationController
 	def sendresults
 	  #duels that have been voted on yesterday
-	  duels = Snap.find_by_sql("select snaps.id, snaps.snapped_by, snaps.photo_url, snaps.vote_left, snaps.vote_right from snaps, votes where snaps.id = votes.snap_id and votes.updated_at >= current_date - interval '1 day'")
+	  #duels = Snap.find_by_sql("select snaps.id, snaps.snapped_by, snaps.photo_url, snaps.vote_left, snaps.vote_right from snaps, votes where snaps.id = votes.snap_id and votes.updated_at >= current_date - interval '1 day'")
+	  
+	  duels = Snap.find_by_sql("select snaps.id, snaps.snapped_by, snaps.photo_url, snaps.vote_left, snaps.vote_right from snaps, votes where snaps.id = votes.snap_id")
 	  
 	  duels.each do |p|
 	  
 	    user = User.find_by_sql("select users.email from users, snaps where snaps.snapped_by = users.id and snaps.id = " + p.id.to_s)
 	    user = user.first
+	    
+	    p.vote_right = Vote.where(:snap_id => p.id, :vote => 'right').count
+      	p.vote_left = Vote.where(:snap_id => p.id, :vote => 'left').count
 	  
 	    client = SendGrid::Client.new(api_user: 'theschnaz', api_key: '33floppyq')
 	      mail = SendGrid::Mail.new do |m|
