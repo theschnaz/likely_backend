fb530868f455b08d3d84c62ca6afca0730225d0f
Gregory Schnese <theschnaz@gmail.com>
2016-01-02 16:16:53 -0500

    working on trending email 5

--- app/controllers/emailresults_controller.rb
+++ app/controllers/emailresults_controller.rb
@@ -40,11 +40,11 @@ class EmailresultsController < ApplicationController
 	
 	  #add a loop here for all users, only sending to theschnaz@gmail.com for now
 	  
-	  url_html = '<table style="width:500px"> <img src="https://dl.dropboxusercontent.com/u/63975/email_logo.png" style="width:500px" /> <br />'
+	  url_html = '<table style="width:500px"> <tr style="padding:bottom:40px;"> <img src="https://dl.dropboxusercontent.com/u/63975/email_logo.png" style="width:500px" /> </tr>'
 	  
 	  #builds the image URLs + html
 	  duels.each do |d|
-	    url_html += '<tr><center><strong>Which is likely ' + d.question.to_s + '?</strong></center></tr> <tr style="padding-bottom:20px;"><img src="' + d.photo_url.to_s + '" /> ' + '</tr>'
+	    url_html += '<tr><center style="font-size:16px;"><strong>Which is likely ' + d.question.to_s + '?</strong></center></tr> <tr style="padding-bottom:40px;"><img src="' + d.photo_url.to_s + '" /> ' + '</tr>'
 	  end
 	  
 	  client = SendGrid::Client.new(api_user: 'theschnaz', api_key: '33sendflop')
