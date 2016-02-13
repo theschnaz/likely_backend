7e8b42efa4f6fa150c18401529c8230994a86411
Gregory Schnese <theschnaz@gmail.com>
2016-02-13 14:46:55 -0500

    tweak 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,17 @@ class SessionsController < ApplicationController
     key = params[:key]
     
     unless User.find_by_uid(uid)
-     user_data = FbGraph2::User.new(uid).authenticate(key)
-     user_data = user_data.fetch(fields: [:name,:email,:gender,:timezone])
-     user = User.new
-     user.name = user_data.name
-     user.facebook_key = params[:key]
-     user.email = user_data.email
-     user.fb_pic_square = 'http://graph.facebook.com/' + user_data.id + '/picture?type=square'
-     user.fb_pic_large = 'http://graph.facebook.com/' + user_data.id + '/picture?type=large'
-     user.provider = 'facebook'
-     user.uid = user_data.id
-     user.save
-    end
-    
-    def new_user_email
-      render :text => 'done' and return
+      user_data = FbGraph2::User.new(uid).authenticate(key)
+      user_data = user_data.fetch(fields: [:name,:email,:gender,:timezone])
+      user = User.new
+      user.name = user_data.name
+      user.facebook_key = params[:key]
+      user.email = user_data.email
+      user.fb_pic_square = 'http://graph.facebook.com/' + user_data.id + '/picture?type=square'
+      user.fb_pic_large = 'http://graph.facebook.com/' + user_data.id + '/picture?type=large'
+      user.provider = 'facebook'
+      user.uid = user_data.id
+      user.save
     end
     
     #if the user already exits, they are sent here becuase of a FB token issue, update token here
@@ -33,5 +29,9 @@ class SessionsController < ApplicationController
     end
     
     render :text => user.name
+  end
+  
+  def new_user_email
+    render :text => 'done' and return
   end
 end
