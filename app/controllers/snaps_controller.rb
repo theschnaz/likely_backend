b373c8f1a00aa0d3d1a804be9a02bde2478005a6
Gregory Schnese <theschnaz@gmail.com>
2016-05-07 18:30:54 -0400

    pages 10

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -41,17 +41,17 @@ class SnapsController < ApplicationController
       end
 
 
-      @url_html += '<tr><td><strong style="font-size:16px;">Which is likely better or worse? <br /><img src="' + @snap.id.to_s + '" style="width:300px;"/> ' + '</td></tr><br/>'
+      @url_html += '<tr><td><strong style="font-size:16px;">Which is likely better or worse? <br /><img src="' + @snap.photo_url.to_s + '" style="width:300px;"/> ' + '</td></tr><br/>'
       @url_html += '<tr><td><strong>Likely better</strong></td></tr>'
       @url_html += '<tr><td>'
       betterthan.each do |x|
-        @url_html += '<a href="/snap/' + x.to_s + '"><img src = "http://res.cloudinary.com/hh55qpw1c/image/upload/w_500,h_500,c_fill/v1419546151/' + x.to_s + '.jpg" style="width:100px;" /></a>'
+        @url_html += '<a href="/snaps/' + x.to_s + '"><img src = "http://res.cloudinary.com/hh55qpw1c/image/upload/w_500,h_500,c_fill/v1419546151/' + x.to_s + '.jpg" style="width:100px;" /></a>'
       end
       @url_html += '</td></tr><br />'
       @url_html += '<tr><td><strong>Likely worse</strong></td></tr>'
       @url_html += '<tr><td>'
       worsethan.each do |x|
-        @url_html += '<a href="/snap/' + x.to_s + '"><img src = "http://res.cloudinary.com/hh55qpw1c/image/upload/w_500,h_500,c_fill/v1419546151/' + x.to_s + '.jpg" style="width:100px;" /></a>'
+        @url_html += '<a href="/snaps/' + x.to_s + '"><img src = "http://res.cloudinary.com/hh55qpw1c/image/upload/w_500,h_500,c_fill/v1419546151/' + x.to_s + '.jpg" style="width:100px;" /></a>'
       end
       @url_html += '</td></tr>'
       @url_html += '<tr ><td style="border-top: 5px solid #cccccc;"><br /><br /></td></tr>'
