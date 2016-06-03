5cf036c8a135dcf585054faa4eeaea2989657d86
Gregory Schnese <theschnaz@gmail.com>
2016-06-02 20:27:28 -0400

    tweaking email for pics you posted

--- app/controllers/emailresults_controller.rb
+++ app/controllers/emailresults_controller.rb
@@ -158,8 +158,9 @@ class EmailresultsController < ApplicationController
 	  
 	  
 	  users.each do |g|
+	  	  puts 'g.id.to_s = ' + g.id.to_s 
 	  	  #my snaps that were voted on yesterday
-		  duels = Vote.connection.select_all("select distinct votes.snap_id, snaps.photo_url, snaps.category from votes, snaps where votes.created_at > CURRENT_DATE - interval '1 day' and snaps.id=votes.snap_id and snaps.snapped_by = " + g.id.to_s + " order by votes.snap_id desc")
+		  duels = Vote.connection.select_all("select distinct votes.snap_id, snaps.photo_url, snaps.category from votes, snaps where votes.created_at > CURRENT_DATE - interval '1 day' and snaps.id = votes.snap_id and snaps.snapped_by = " + g.id.to_s + " order by votes.snap_id desc")
 		  
 		  if(duels.count == 0)
 		  	render :text => "no new votes" and return
